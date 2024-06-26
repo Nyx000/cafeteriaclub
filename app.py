16b2df815f101c6150abdfa0ff2154040ac1cf77
Nathan <husenathan@gmail.com>
2024-06-26 04:23:21 -0700

    Updated app.py to run on 0.0.0.0 with dynamic port

--- app.py
+++ app.py
@@ -1,12 +1,9 @@
 # Standard library imports
 import logging
-from datetime import datetime
 import os
 
 # Third-party library imports
-from flask import Flask, render_template, jsonify, request
-import json
-from user_agents import parse
+from flask import Flask, render_template, jsonify
 
 app = Flask(__name__)
 
@@ -16,10 +13,6 @@ def get_menu_data():
         {"id": 2, "name": "Pizza", "price": 10.99},
         {"id": 3, "name": "Salad", "price": 6.99}
     ]
-
-    
-    log_message = json.dumps(visit_data)
-    logging.info(log_message)
 
 @app.route('/')
 def home():
@@ -31,5 +24,9 @@ def get_menu():
     return jsonify(menu)
 
 if __name__ == '__main__':
+    # Configure logging
+    logging.basicConfig(level=logging.INFO)
+    
     port = int(os.environ.get('PORT', 5000))
     app.run(host='0.0.0.0', port=port)
+
